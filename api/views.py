f7c7f6aaf3d715f2c723be56a77a9976c1b6f816
Pim van Schayik <pimvschayik@live.nl>
2020-09-07 21:43:07 +0100

    Cors, allowed hosts, dummydata

--- api/views.py
+++ api/views.py
@@ -6,8 +6,39 @@ import json
 
 def index(request):
 
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    summonerNames = body['data']
+    print(summonerNames)
+
     api_key = config('RIOT_KEY')
     response = requests.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Schayik?api_key=' + api_key)
     data = response.json()
+    print(data)
 
-    return HttpResponse(json.dumps(data))
+
+    dummyData = {
+        'gameCount': 10,
+        'totalKills': 23,
+        'totalDamage': 24,
+        'ksScoreData': [
+            {
+                'summonerName': 'Djep0', 
+                'ksScore': 1, 
+                'kills': 12, 
+                'killPercentage': 32,
+                'damage': 100, 
+                'damagePercentage': 24
+            },
+            {
+                'summonerName': 'Snitsky', 
+                'ksScore': 1.2, 
+                'kills': 34, 
+                'killPercentage': 34,
+                'damage': 123, 
+                'damagePercentage': 51
+            }
+        ]
+    }
+
+    return HttpResponse(json.dumps(dummyData))
