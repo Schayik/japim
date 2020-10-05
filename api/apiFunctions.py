998e4a879b137ba789217cc4b221918aea0bce84
JasperH93 <jasperhermsen@hotmail.com>
2020-10-05 23:13:11 +0200

    Update apiFunctions.py
    Response code afvanging

--- api/apiFunctions.py
+++ api/apiFunctions.py
@@ -3,14 +3,42 @@ from django.http import HttpResponse
 from decouple import config
 import requests
 import json
+import time
 
-def Invoke_API (method,request,extras=''):
+def Invoke_API (method,request,extras='',retryTimeOut=0,safetyValue=0,r=''):
+    if safetyValue > 50:
+        print('safetyValue exceeded, throw! value: ', safetyValue)
+        if r != '':
+            r.raise_for_status()     ## THROWS ERROR
+        else:
+            raise ValueError('Recursive safetyValue exceeded by unknown error')     ## THROWS ERROR
+    if retryTimeOut > 0:
+        print('timed out, timer, safety: ',retryTimeOut,', ', safetyValue)
+        time.sleep(retryTimeOut)
+        
     baseURL = 'https://euw1.api.riotgames.com/'
     apiKey = config('RIOT_KEY')
     requestURL = baseURL + method + request + '?api_key=' + apiKey
     response = requests.get(requestURL)
+    
+    if response.status_code == 200:
+        pass
+        print('Call succesfull')
+    elif response.status_code == 429:  
+        retryTimeOutStr = response.headers['retry-after']
+        safetyValue+=1
+        print('429 error, calling recursive with retry, safety: ',retryTimeOutStr,', ', safetyValue)
+        response = Invoke_API(method,request,extras,int(retryTimeOutStr),safetyValue,response)
+        return response
+    else:
+        safetyValue+=26
+        retryTimeOutStr = '10'
+        print('different error: ', response.status_code, ' safety: ', safetyValue) 
+        response = Invoke_API(method,request,extras,int(retryTimeOutStr),safetyValue,response)
+        return response
+    
+    print('end of function returning response, response value = ',response)
     responseData = response.json()
-    #print('api1',responseData)
     return responseData
 
 def Get_AccountID (summonerName):
